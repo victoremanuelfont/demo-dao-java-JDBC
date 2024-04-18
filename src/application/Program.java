f36850fc6ed9e8a69e00e59f4a5f410e3c924b24
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-04-18 12:06:02 -0300

    Criando a classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Department dp = new Department(1, "Victor");
-		System.out.println(dp);
+		Seller se = new Seller(1, "Victor Emanuel", "Victor@gmail.com", new Date(), 3000.00, dp);
+		System.out.println(se);
 
 	}
 
