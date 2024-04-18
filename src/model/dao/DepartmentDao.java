9e0802941a3ab5ef7820615fede861c37f5753d0
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-04-18 12:50:28 -0300

    Criando interfaces DepartmentDao e SellerDao

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,24 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Department;
+
+public interface DepartmentDao {
+
+	void insert(Department obj); // insert recebendo um department como argumento
+
+	void update(Department obj);
+
+	void deleteById(Integer id);
+
+	Department findById(Integer id); 	/* Retorna um Department e recebe como argumento um id. Essa operação é
+										 * responsável por pegar esse ID e  consultar no banco de dados o objeto com o
+										 * mesmo ID, se existir, retorna o Department, se não, retorna nulo.
+										 */
+	
+	
+	List<Department> findAll();	// Retorna uma lista de Departamentos
+	
+
+}
---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,22 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Seller;
+
+public interface SellerDao {
+	void insert(Seller obj); // insert recebendo um Seller como argumento
+
+	void update(Seller obj);
+
+	void deleteById(Integer id);
+
+	Seller findById(Integer id);    /* Retorna um Seller e recebe como argumento um id. Essa operação é responsável
+									 * por pegar esse ID e consultar no banco de dados o objeto com o mesmo ID, se
+									 * existir, retorna o Seller, se não, retorna nulo.
+									 */
+	
+	
+	List<Seller> findAll();	// Retorna uma lista de Seller 
+
+}
