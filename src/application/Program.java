64e4c7413fd3f82af5417f47b755b89e8a61d4ea
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-04-18 16:41:22 -0300

    Implementando Insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -28,6 +29,10 @@ public class Program {
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
+		System.out.println("\n==== TEST 4: seller findAll =====");
+		Seller newSeller = new Seller(null, "Joao", "Joao@outlok.com", new Date(), 4000.00, department);
+		sellerDao.insert(newSeller);
+		System.out.println("inserido, novo ID = " + newSeller.getId());
 		
 	}
 
